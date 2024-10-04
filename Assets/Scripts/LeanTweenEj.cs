8008d853c30d047fd0bf883f1edf9b1c47a38f38
Dariaa33 <vesnayoon@gmail.com>
2024-10-04 08:52:08 +0000

    added Pause and Play

--- Assets/Scripts/LeanTweenEj.cs
+++ Assets/Scripts/LeanTweenEj.cs
@@ -10,16 +10,21 @@ public class LeanTweenEj : MonoBehaviour
     float durationAnim = 0.75f;
     [SerializeField]
     float finalYPose = 0.5f;
+    [SerializeField]
+    float finalXPose = -2f;
 
     // Start is called before the first frame update
     void Start()
     {
         LeanTween.moveLocalY(gameObject, 0.5f, durationAnim).setEase(LeanTweenType.easeInOutElastic);
+        LeanTween.moveLocalX(gameObject, -2f, durationAnim).setEase(LeanTweenType.easeInOutBounce);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+       
     }
 }
--- Assets/Scripts/PopUp.cs
+++ Assets/Scripts/PopUp.cs
@@ -20,13 +20,44 @@ public class PopUp : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            background.SetActive(true);
-            popup.SetActive(true);
-            LeanTween.moveLocalY(popup, -1100f, 0f);
-            LeanTween.moveLocalY(popup, 0f, timeAnim).setEase(LeanTweenType.animationCurve);
+       if (Input.GetKeyUp(KeyCode.Escape))
 
+        {
+            if (popup.activeSelf)
+            {
+                HidePopUp();
+            }
+
+            else
+            {
+                ShowPopUp();
+            }
         }
+    }
+
+    public void ShowPopUp()
+    {
+        background.SetActive(true);
+        popup.SetActive(true);
+        LeanTween.moveLocalY(popup, -1100f, 0f);
+        LeanTween.moveLocalY(popup, 0f, timeAnim).setEase(LeanTweenType.animationCurve);
+    }
+
+    public void HidePopUp()
+    {
+        LeanTween.moveLocalY(popup, -900f, timeAnim).setEase(LeanTweenType.animationCurve).setOnComplete(() =>
+        {
+            popup.SetActive(false);
+
+        });
+        LeanTween.alphaCanvas(background.GetComponent<CanvasGroup>(), 0f, timeAnim).setOnComplete(() =>
+        {
+            background.SetActive(false);
+            LeanTween.alphaCanvas(background.GetComponent<CanvasGroup>(), 1f, timeAnim);
+
+        });
+            
+        
+        
     }
 }
